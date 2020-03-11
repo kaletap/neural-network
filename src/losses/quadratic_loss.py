916538973400556166a2723500729b3c61aec6e9
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-03-11 12:53:39 +0100

    Backward method of QuadraticLoss with tests of dimensionality.

--- src/losses/quadratic_loss.py
+++ src/losses/quadratic_loss.py
@@ -6,7 +6,7 @@ from .base_loss import Loss
 class QuadraticLoss(Loss):
     """
     Loss for simple regression: squared error.
-    For `n` observations it will produce n errors.
+    For `n` observations it will produce n error values.
     """
 
     def forward(self, y_predicted: np.ndarray, y_true: np.ndarray):
@@ -17,4 +17,4 @@ class QuadraticLoss(Loss):
     def backward(self, y_predicted: np.ndarray, y_true: np.ndarray):
         assert y_true.shape[0] == y_predicted.shape[0] == 1
         assert y_true.shape[0] == y_predicted.shape[0]
-        pass
+        return 2 * (y_predicted - y_true)
--- test/test_losses.py
+++ test/test_losses.py
@@ -8,3 +8,11 @@ def test_quadratic_loss():
     y_true = np.ones([1, n])
     y_predicted = np.zeros([1, n])
     assert (QuadraticLoss()(y_predicted, y_true) == np.ones([1, n])).all()
+
+
+def test_quadratic_loss_backward():
+    n = 20
+    y_true = np.ones([1, n])
+    y_predicted = np.zeros([1, n])
+    loss = QuadraticLoss()
+    assert loss.backward(y_predicted, y_true).shape == (1, n)
